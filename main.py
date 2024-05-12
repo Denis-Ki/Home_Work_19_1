ee92b74c6fa4580b6cd88b551a8022a4d72632de
Denis Ki <duzzz@bk.ru>
2024-05-12 08:45:29 +0300

    add main.py and edit (exercise 2)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
-# Для начала определим настройки запуска
 hostName = "localhost" # Адрес для доступа по сети
 serverPort = 8080 # Порт для доступа по сети
 
